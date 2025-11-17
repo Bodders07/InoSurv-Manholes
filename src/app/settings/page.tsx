2c35e8e172284a54b8471384612fd3dabb5ca456
jack boardman <jackboardman09@gmail.com>
2025-11-17 08:09:01 +0000

    wrap settings in suspense and polish login/header

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,12 +1,15 @@
 'use client'
 
+import { Suspense } from 'react'
 import SidebarLayout from '@/app/components/SidebarLayout'
 import SettingsContent from './SettingsContent'
 
 export default function SettingsPage() {
   return (
     <SidebarLayout>
-      <SettingsContent />
+      <Suspense fallback={<div>Loading settings...</div>}>
+        <SettingsContent />
+      </Suspense>
     </SidebarLayout>
   )
 }
